be20952f08c554e8b2ffae5572219f95a2c10c93
raghav69-crypt <raghavgupta241@gmail.com>
2024-01-23 19:20:45 +0530

    Update Double LL

--- Greedy/test.cpp
+++ Greedy/test.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-using namespace name
-int main(){
+using namespace std;
+int main()
+{
 int sum=0;
 int a,b;
 cin>>a>>b;
 sum = a+b;
 cout<<"sum of the digits is = "<<sum;
 return 0;
-
 }
