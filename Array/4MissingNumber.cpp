2ff03d43e7df5b0d2f192a74980533d94bddba24
Raghav Gupta <74757186+raghav69-crypt@users.noreply.github.com>
2024-03-12 15:47:42 +0530

    Update 4MissingNumber.cpp

--- Array/4MissingNumber.cpp
+++ Array/4MissingNumber.cpp
@@ -3,6 +3,6 @@ class Solution
     public:
       int missingNumber(vector<int> &nums) {
         long n = nums.size();
-    return n * (n+1) / 2 - accumulate(begin(nums), end(nums), 0);  
+    return n * (n+1) / 2 - accumulate(begin(nums), end(nums), 0);  // Subtraction of sum of numbers and sum of n numbers : Returns the solution
       }
 };
